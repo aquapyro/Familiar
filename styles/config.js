8e331e90f369c19b3c8d2328a476c0f15b80bc66
aquapyro <65801565+aquapyro@users.noreply.github.com>
2024-04-20 14:08:43 -0700

    Add files via upload

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1XwS-3WBxCM_DBwz-kbnq_2xHnsc8ccbU7j2ib2ZRRXA";
 
 
 /* ------------------------------------------------------------------- */
